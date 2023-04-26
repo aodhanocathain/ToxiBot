2a67153490e6622aa2ca64d723ff2db5c3b89a1a
Aodhán Keane <78481710+aodhanocathain@users.noreply.github.com>
2023-04-26 08:12:55 +0100

    Remove old test.js

--- commands/test.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports = {
-	discordCommand : new Discord.SlashCommandBuilder().setName("test").setDescription("Prompts acknowledgement by the bot"),
-	execute: (interaction)=>{interaction.reply("Test");}
-}
