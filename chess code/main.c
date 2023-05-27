065940b2679ee018b6dc90ac9d8f26a0687a7b95
unknown <aodhanocathain@gmail.com>
2023-05-27 19:50:56 +0100

    remove queenside from castlerights

--- chess code/main.c
+++ chess code/main.c
@@ -3,6 +3,6 @@
 int main()
 {
 	//printf("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1\n");
-	printf("4k3/8/8/8/8/8/8/4K3 w KQkq - 0 1\n");
+	printf("4k3/8/8/8/8/8/8/4K3 w Kk - 0 1\n");
 	return 0;
 }
