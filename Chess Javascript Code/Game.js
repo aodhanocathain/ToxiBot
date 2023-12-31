4ccbc01d7f220d041765586c74fcbff2abe38e37
unknown <aodhanocathain@gmail.com>
2023-12-31 12:14:18 +0000

    add more unit tests

--- Chess Javascript Code/Game.js
+++ Chess Javascript Code/Game.js
@@ -11,7 +11,7 @@ const {Worker, isMainThread, parentPort} = require("worker_threads");
 
 const numCPUs = require("os").cpus().length;
 
-const DEFAULT_ANALYSIS_DEPTH = 3;
+const DEFAULT_ANALYSIS_DEPTH = 1;
 
 //fancy analysis time is approximately (branch factor)^(depth + foresight) but foresight is more expensive than depth
 const FANCY_ANALYSIS_DEPTH = 3;
@@ -78,18 +78,6 @@ class Game
 	//static DEFAULT_FEN_STRING = "1nb1kbn1/8/8/8/8/8/8/1NB1KBN1 w KQkq - 0 1";	//king,knight,bishop game
 	//static DEFAULT_FEN_STRING = "1n2k1n1/8/8/8/8/8/8/1N2K1N1 w KQkq - 0 1";	//king,knight game
 	//static DEFAULT_FEN_STRING = "4k3/8/8/8/8/8/8/4K3 w KQkq - 0 1";	//king game
-	
-	//static DEFAULT_FEN_STRING = "8/4Br2/8/8/8/8/p1P5/k1Kb4 w - - 0 1";	//win the rook in 9ish halfmoves
-	
-	//static DEFAULT_FEN_STRING = "4k3/6P1/8/8/8/8/1p6/4K3 w KQkq - 0 1";	//promotion test
-	//static DEFAULT_FEN_STRING = "4k3/6P1/8/8/8/8/1p6/B3K3 b KQkq - 0 1";	//capture with promotion test
-	
-	//static DEFAULT_FEN_STRING = "rnbqkbnr/ppp2ppp/4p3/3pP3/8/8/PPPP1PPP/RNBQKBNR w - d 0 3";	//en passant test
-	//static DEFAULT_FEN_STRING = "4k3/8/4p3/3pP3/8/8/8/4K3 w - d 0 3";	//en passant test
-		
-	//static DEFAULT_FEN_STRING = "k7/8/K7/Q7/8/8/8/8 w KQkq - 0 1";	//white gives checkmate in 1 test (evaluate at 1)
-	//static DEFAULT_FEN_STRING = "3k4/5Q2/K7/8/8/8/8/8 b - - 0 1";	//white gives checkmate in 2 test (evaluate at 5)
-	//static DEFAULT_FEN_STRING = "b6K/4qq2/8/8/8/8/Q7/7k w KQkq - 0 1";	//stalemate test (queen sac) (evaluate at 3)
 	
 	pieces;	//indexed by a square on the board
 	
--- Game.test.js
+++ Game.test.js
@@ -1,16 +1,31 @@
 const {Game} = require("./Chess Javascript Code/Game.js");
 
 //static DEFAULT_FEN_STRING = "8/4Br2/8/8/8/8/p1P5/k1Kb4 w - - 0 1";	//win the rook in 9ish halfmoves
-	
-//static DEFAULT_FEN_STRING = "4k3/6P1/8/8/8/8/1p6/4K3 w KQkq - 0 1";	//promotion test
-//static DEFAULT_FEN_STRING = "4k3/6P1/8/8/8/8/1p6/B3K3 b KQkq - 0 1";	//capture with promotion test
-
-//static DEFAULT_FEN_STRING = "rnbqkbnr/ppp2ppp/4p3/3pP3/8/8/PPPP1PPP/RNBQKBNR w - d 0 3";	//en passant test
-//static DEFAULT_FEN_STRING = "4k3/8/4p3/3pP3/8/8/8/4K3 w - d 0 3";	//en passant test
-	
-//static DEFAULT_FEN_STRING = "3k4/5Q2/K7/8/8/8/8/8 b - - 0 1";	//white gives checkmate in 2 test (evaluate at 5)
-//static DEFAULT_FEN_STRING = "b6K/4qq2/8/8/8/8/Q7/7k w KQkq - 0 1";	//stalemate test (queen sac) (evaluate at 3)
 
 test("find checkmate in 1", ()=>{
 	expect(new Game("k7/8/K7/Q7/8/8/8/8 w KQkq - 0 1").evaluate(2).bestMove.toString()).toBe("Qd8#");
+});
+
+test("find checkmate in 2", ()=>{
+	expect(new Game("2k5/5Q2/K7/8/8/8/8/8 w - - 1 2").evaluate(4).bestMove.toString()).toBe("Qe7");
+});
+
+test("find queen sac for stalemate", ()=>{
+	expect(new Game("b6K/4qq2/8/8/8/8/Q5p1/7k w KQkq - 0 1").evaluate(3).bestMove.toString()).toBe("Qxg2+");
+});
+
+test("find en passant pt1", ()=>{
+	expect(new Game("4k3/8/4p3/3pP3/8/8/8/4K3 w - d 0 3").evaluate(1).bestMove.toString()).toBe("exd6");
+});
+
+test("find en passant pt2", ()=>{
+	expect(new Game("rnbqkbnr/ppp2ppp/4p3/3pP3/8/8/PPPP1PPP/RNBQKBNR w - d 0 3").evaluate(1).bestMove.toString()).toBe("exd6");
+});
+
+test("pawn promotion by capture", ()=>{
+	expect(new Game("4k3/6P1/8/8/8/8/1p6/B3K3 b KQkq - 0 1").evaluate(1).bestMove.toString()).toBe("bxa1=Q+");
+});
+
+test("pawn promotion", ()=>{
+	expect(new Game("4k3/6P1/8/8/8/8/1p6/4K3 w KQkq - 0 1").evaluate(1).bestMove.toString()).toBe("g8=Q+");
 });
